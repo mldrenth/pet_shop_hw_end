3f78ec83d491d5a3310604049c77261e38bf6da5
Marijke Drenth <drenth.marijke@googlemail.com>
2021-11-12 15:39:21 +0200

    Added functionality to add/remove cash and get amount of pets sold

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,3 +5,12 @@ def get_pet_shop_name(pet_shop):
 
 def get_total_cash(pet_shop):
      return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop,cash_amount):
+    current_total_cash = pet_shop["admin"]["total_cash"]
+    pet_shop["admin"]["total_cash"] = current_total_cash + cash_amount
+    
+    return current_total_cash
+
+def get_pets_sold(pet_shop):
+    return  pet_shop["admin"]["pets_sold"]
